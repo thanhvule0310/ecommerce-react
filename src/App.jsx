0071d607f306fd849b9eca0f06eb1c76c4c3fbda
Lê Thanh Vũ <thanhvule0310@gmail.com>
2020-01-13 09:53:48 +0700

    Adding header :card_index:

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 
+import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Shop from './pages/Shop/Shop';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/shop" component={Shop} />
