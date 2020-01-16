3e8819e0fbd6ef6a8b3a6bef79d9637c275d7c51
Lê Thanh Vũ <thanhvule0310@gmail.com>
2020-01-16 11:03:20 +0700

    Remove Alert in Home :clown_face:

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import './Home.scss';
 
 import Directory from '../../components/Directory/Directory';
-import Alert from '../../components/UI/Alert/Alert';
 
 const Home = () => (
   <div className="homepage">
-    <Alert>Success</Alert>
     <Directory />
   </div>
 );
